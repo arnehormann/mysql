c947c343dc1e359f592a6e2b46cae16b2f2ca3b6
Arne Hormann <arnehormann@gmail.com>
2015-03-27 10:15:49 +0100

    added test helpers and tests for #314

---
+++ helpers_test.go
@@ -0,0 +1,85 @@
+package mysql
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+	"testing"
+	"time"
+)
+
+// Printer is a function that can be used as a Logger.
+type Printer func(v ...interface{})
+
+func (p Printer) Print(v ...interface{}) {
+	p(v...)
+}
+
+// useLogger sets the logger and returns a function resetting it to the old one.
+// Use it as
+//     defer useLogger(LOGGER)()
+func useLogger(p Printer) func() {
+	var logger = errLog
+	SetLogger(p)
+	return func() {
+		SetLogger(logger)
+	}
+}
+
+// testTimebound runs a test and fails if it takes longer than allowed.
+func testTimebound(t *testing.T, timeout time.Duration, f func(*testing.T)) {
+	timer := time.After(timeout)
+	done := make(chan struct{})
+	defer close(done)
+	go func() {
+		f(t)
+		done <- struct{}{}
+	}()
+	select {
+	case <-timer:
+		t.Fatal("TIMEOUT")
+	case <-done:
+	}
+}
+
+// tracer is useable as a Logger, prints full stacktrace of all package internal calls
+type tracer struct{}
+
+func (t tracer) Print(v ...interface{}) {
+	trace := []string{}
+	var (
+		dstPkg string
+		file   string
+		line   int
+	)
+	ok := true
+	for i := 0; ok; i++ {
+		_, file, line, ok = runtime.Caller(i)
+		if !ok {
+			break
+		}
+		pkg := ""
+		pEnd := 0
+		for j := len(file) - 2; j > 0; j-- {
+			if file[j] == '/' {
+				if pEnd != 0 {
+					pkg = file[j+1 : pEnd]
+					if i == 0 {
+						dstPkg = pkg
+					}
+					file = file[j+1:]
+					break
+				}
+				pEnd = j
+			}
+		}
+		if pkg == dstPkg && i > 0 {
+			trace = append(trace, fmt.Sprintf("%s:%d", file, line))
+		}
+	}
+	fmt.Fprintln(
+		os.Stderr,
+		append([]interface{}{"[" + strings.Join(trace, ", ") + "]: "}, v...),
+	)
+}
---
+++ issue314_test.go
@@ -0,0 +1,65 @@
+package mysql
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"testing"
+	"time"
+)
+
+// test for issue #314:
+// Busy buffer. Commands out of sync. Did you run multiple statements at once?
+
+func test314Text(t *testing.T) {
+	dsts := []driver.Value{sql.NullInt64{}}
+	db, err := MySQLDriver{}.Open(dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+	conn, _ := db.(*mysqlConn)
+	rows, err := conn.Query("SELECT 1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	conn.Close()
+	rows.Next(dsts) // busy buffer
+	rows.Close()
+}
+
+func test314Binary(t *testing.T) {
+	dsts := []driver.Value{sql.NullInt64{}}
+	conn, err := MySQLDriver{}.Open(dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+	stmt, err := conn.Prepare("SELECT 1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	rows, err := stmt.Query(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	stmt.Close() // busy buffer
+	rows.Next(dsts)
+	rows.Close() // freeze
+	conn.Close()
+}
+
+func Test314Text(t *testing.T) {
+	if !available {
+		t.Skipf("MySQL-Server not running on %s", netAddr)
+	}
+	defer useLogger(t.Error)()
+	testTimebound(t, 1*time.Second, test314Text)
+}
+
+func Test314Binary(t *testing.T) {
+	if !available {
+		t.Skipf("MySQL-Server not running on %s", netAddr)
+	}
+	defer useLogger(t.Error)()
+	testTimebound(t, 1*time.Second, test314Binary)
+}
